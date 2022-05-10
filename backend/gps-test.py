c6ec0bfc1ed82441a271d34f852c763edef79e6f
fantasyland101 <40237106+fantasyland101@users.noreply.github.com>
2022-05-10 14:01:39 +0200

    Update gps-test.py

--- backend/gps-test.py
+++ backend/gps-test.py
@@ -1,1 +1,14 @@
+# code from https://github.com/MartijnBraam/gpsd-py3/blob/master/README.rst  
+import gpsd
 
+# Connect to the local gpsd
+gpsd.connect()
+
+# Connect somewhere else
+gpsd.connect(host="127.0.0.1", port=123456)
+
+# Get gps position
+packet = gpsd.get_current()
+
+# See the inline docs for GpsResponse for the available data
+print(packet.position())
